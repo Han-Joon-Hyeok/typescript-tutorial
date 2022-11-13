ae8b7013bd943091751abc27d8360ae2f000bf52
joonhyuk.han <virq@naver.com>
2022-11-13 16:50:30 +0900

    Use interface with fucntion

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ var message = "Hello, TypeScript!";
 var heading = document.createElement('h1');
 heading.textContent = message;
 document.body.appendChild(heading);
+var getProduct = function (id) {
+    return {
+        id: id,
+        name: "Product ".concat(id),
+        price: 42
+    };
+};
+var showProduct = function (name, price) {
+    console.log("The product ".concat(name, " costs ").concat(price));
+};
+var product = getProduct(1);
+showProduct(product.name, product.price);
--- app.ts
+++ app.ts
@@ -1,5 +1,28 @@
+// Section1. TypeScript Hello World
 let message: string = "Hello, TypeScript!";
 
 let heading = document.createElement('h1');
 heading.textContent = message;
 document.body.appendChild(heading);
+
+// Section1. Why TypeScript
+interface Product {
+	id: Number,
+	name: String,
+	price: Number
+}
+
+const getProduct = (id: Number): Product => {
+	return {
+		id: id,
+		name: `Product ${id}`,
+		price: 42
+	}
+} 
+
+const showProduct = (name: String, price: Number) => {
+	console.log(`The product ${name} costs ${price}`);
+};
+
+const product = getProduct(1);
+showProduct(product.name, product.price); // The product Product 1 costs 42
